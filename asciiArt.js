326877cc254947020c0cc830d4bfe1cb1be510a9
jeremDev <jeremtaylor@gmail.com>
2014-04-28 18:47:08 -0400

    Change variable scopes
    Some variables were defined as class-level properties, when they were
    only used in one method, so I made them local to the method.

--- asciiArt.js
+++ asciiArt.js
@@ -3,11 +3,8 @@
 asciiGenerator = function() {
 	this.width;
 	this.height;
-	this.regex = new RegExp(/[^A-Z]/g);	// Used to replace any non-alpha characters with a question mark
 	this.text;
 	this.ascii = new Array();
-	this.row;
-	this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?".split("");
 	
 	this.init = function() {
 		this.width = readline();	//read 1
@@ -16,17 +13,20 @@ asciiGenerator = function() {
 	
 	this.readText = function() {
 		// Read in the string, make it upper case, replace any non-alpha characters with question marks, and put each character into an array
+		var regex = new RegExp(/[^A-Z]/g);	// Used to replace any non-alpha characters with a question mark
 		this.text = readline().toUpperCase().replace(this.regex,"?").split("");	//read 3
 	}
 	
 	this.readAsciiMappings = function() {
+		var row;
+		var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?".split("");
 		// Loop once for each row that will be read in
 		for (i = 0; i < this.height; i++) {
-			this.row = readline();		//read 4
+			row = readline();		//read 4
 			this.ascii[i] = new Array();
-			// Loop over the alphabet, chopping up the row into array elements of the defined width, setting each letter as a key in the array
-			for(j = 0; j < this.alphabet.length; j++) {
-				this.ascii[i][this.alphabet[j]] = this.row.substr(j * this.width,this.width);
+			// Loop once for each letter of the alphabet + '?', chopping up the row into array elements of the defined width, setting each letter as a key in the array
+			for(j = 0; j < alphabet.length; j++) {
+				this.ascii[i][alphabet[j]] = row.substr(j * this.width,this.width);
 			}
 		}
 	}
